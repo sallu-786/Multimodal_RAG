8930f23d0feb9646754df38ed2daac3d6f5b403e
sallu-786 <166565@toyota-boshoku.co.jp>
2024-09-17 19:16:28 +0900

    Update get_response.py

--- utils/get_response.py
+++ utils/get_response.py
@@ -1,58 +1,29 @@
-import os
-import streamlit as st
-
-
-
-
-#For Azure Open Ai---------------------------------------------------------
-from openai import AzureOpenAI
-endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-api_key = os.getenv("AZURE_OPENAI_API_KEY")
-client = AzureOpenAI(api_version="")
-model = ""
-
-system_msg = (
-            """"
+from langchain_ollama import ChatOllama
+system_msg = ( """"
            You are a helpful Assistant named TB Chat.
            If some documents given to you, You always Answer the questions properly based on the provided document.
            If the information is not in the documents or you can't find it, 
            then give your own information based answer. Don't hallucinate """
         )
+def response_ollama(user_message: str, input_documents, chat_history: list=[], my_model="llama3"):
+    model = ChatOllama(model=my_model, temperature=0)
+    my_message = [{"role": "system", "content": system_msg}]
 
-
-
-
-
-# Function to get response from LLM model (Chat-GPT-Azure)---------------------------------------------------
-
-
-def response_chatgpt_az(message: str, input_documents, chat_history: list = []):
-
-    messages = [{"role": "system", "content": system_msg}]
-
-    # Add chat history
-    for chat in chat_history[-2:]:
+    for chat in chat_history[-1:]:
         if isinstance(chat, dict) and "role" in chat and "content" in chat:
-            messages.append(chat)
+           my_message.append(chat)
+                                                     #Append the latest question in message
+    my_message.append({"role": "user", "content": user_message})
 
-    # Add user message
-    messages.append({"role": "user", "content": message})
-
-    # Add input documents
+        # Add input documents
     for doc in input_documents:
         if isinstance(doc, dict) and "content" in doc:
-            messages.append({"role": "user", "content": f"Document snippet:\n{doc['content']}"})
-
-    try:
-        response = client.chat.completions.create(model=model, messages=messages, temperature=0)
-        return {
-            "answer": response.choices[0].message.content,
-            "sources": input_documents,
-            
-        }
-    except Exception as e:
-        st.error(f"Could not find LLM model: {str(e)}")
-        return None
+            my_message.append({"role": "user", "content": f"Document snippet:\n{doc['content']}"})
+    
+    response = model.invoke(                          #define model&input for response 
+        my_message
+        )
+    return response.content
 
 
 
