658fdbf864ff735ec0d084bc9db14cef0843e325
sallu-786 <166565@toyota-boshoku.co.jp>
2024-09-17 19:20:57 +0900

    Update url.py

--- utils/url.py
+++ utils/url.py
@@ -1,17 +1,13 @@
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import AzureOpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 import os 
 from dotenv import load_dotenv
 from langchain.retrievers import EnsembleRetriever
 load_dotenv()
 
-
-model = "your_model_name"
-endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-api_key = os.getenv("AZURE_OPENAI_API_KEY")
-
+model="intfloat/multilingual-e5-large" 
 
 def url_chunks(target_url):
     loader = WebBaseLoader(target_url)
@@ -21,7 +17,6 @@ def url_chunks(target_url):
         chunk_overlap=10,
         length_function=len
     )
-
     splited_document = text_splitter.split_documents(raw_document)
     return splited_document
 
@@ -29,15 +24,13 @@ def rag_with_url(target_url,query):
    
     loader = WebBaseLoader(target_url)
     raw_document = loader.load()
-    embeddings = AzureOpenAIEmbeddings(azure_deployment=model, openai_api_version="2023-05-15")
+    embeddings = HuggingFaceEmbeddings(model_name=model)
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=10,
         length_function=len
     )
-
     splited_document = text_splitter.split_documents(raw_document)
-
     vector_store = FAISS.from_documents(splited_document, embeddings)
     faiss_retriever = vector_store.as_retriever(search_kwargs={"k": 3})
     ensemble_retriever = EnsembleRetriever(
